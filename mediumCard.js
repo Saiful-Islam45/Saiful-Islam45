49720b490189efa90f78883e705fd9a4c8317525
Saiful-Islam45 <saiful.islam.ice.45@gmail.com>
2023-11-20 22:35:53 +0600

    update axios

--- mediumCard.js
+++ mediumCard.js
@@ -1,41 +1,50 @@
-import fetch from 'node-fetch';
-import fs from 'fs';
+const axios = require('axios');
+const fs = require('fs');
 
-const mediumUsername = 'yourusername';
+const mediumUsername = '@saiful-islam-ice-45';
 
 async function fetchLatestMediumPost() {
-  const response = await fetch(`https://medium-rss-badge.vercel.app/api/user/${mediumUsername}`);
-  const data = await response.json();
-  console.log("data:", data);
+  try {
+    const response = await axios.get(`https://medium-rss-badge.vercel.app/api/user/${mediumUsername}`);
+    const data = response.data;
 
-  const latestPost = data.items[0]; // Assuming the first item is the latest post
+    const latestPost = data.items[0]; // Assuming the first item is the latest post
 
-  return {
-    title: latestPost.title,
-    image: latestPost.thumbnail,
-    url: latestPost.link,
-  };
+    return {
+      title: latestPost.title,
+      image: latestPost.thumbnail,
+      url: latestPost.link,
+    };
+  } catch (error) {
+    console.error('Error fetching Medium post:', error.message);
+    throw error;
+  }
 }
 
 async function updateReadme() {
-  // Read existing README content
-  const existingReadme = fs.readFileSync('README.md', 'utf-8');
+  try {
+    // Read existing README content
+    const existingReadme = fs.readFileSync('README.md', 'utf-8');
 
-  // Fetch latest Medium post information
-  const { title, image, url } = await fetchLatestMediumPost();
+    // Fetch latest Medium post information
+    const { title, image, url } = await fetchLatestMediumPost();
 
-  // Construct updated content
-  const updatedContent = `
-  # Latest Blog Post
-  [![Latest Blog Post](${image})](${url})
-  
-  Read the full post: [${title}](${url})
-  
-  ${existingReadme}
-  `;
+    // Construct updated content
+    const updatedContent = `
+    # Latest Blog Post
+    [![Latest Blog Post](${image})](${url})
+    
+    Read the full post: [${title}](${url})
+    
+    ${existingReadme}
+    `;
 
-  // Write updated content back to README
-  fs.writeFileSync('README.md', updatedContent);
+    // Write updated content back to README
+    fs.writeFileSync('README.md', updatedContent);
+  } catch (error) {
+    console.error('Error updating README:', error.message);
+    throw error;
+  }
 }
 
 updateReadme();
